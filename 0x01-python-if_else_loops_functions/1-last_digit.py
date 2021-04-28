fa84f801a5517cdaf65fc17aca134ba050eff833
joseheco <jferhc@gmail.com>
2021-04-27 19:50:57 -0500

    lastDigit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,13 +5,11 @@ if number > 0:
     lastNumber = number % 10
 else:
     lastNumber = number % -10
-
 if lastNumber == 0:
     retorna = "and is 0"
 elif lastNumber > 5:
     retorna = "and is greater than 5"
 else:
     retorna = "and is less than 6 and not 0"
-
 print("Last digit of {:d} is {:d} {}".format(number,lastNumber,retorna))
 
