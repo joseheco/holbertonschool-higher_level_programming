af362ec9062262d44bcec81db2294cd453d80a5d
joseheco <jferhc@gmail.com>
2021-05-05 18:31:33 -0500

    result

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@
 
 def divisible_by_2(my_list=[]):
     resultado = []
-    if resultado:
+    if my_list:
         for idx in range(0, len(my_list)):
             resultado.append(False if my_list[idx] % 2 else True)
     return resultado
