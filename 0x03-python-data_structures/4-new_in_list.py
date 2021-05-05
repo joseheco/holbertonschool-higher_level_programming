d51aeff359cf3e94ab1e8f6150b945d892e5e8b7
joseheco <jferhc@gmail.com>
2021-05-04 23:50:34 -0500

    list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def mew_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx < 0 or idx >= len(my_list):
         return(new_list)
