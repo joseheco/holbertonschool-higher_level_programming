9cbc64a316174c7e54f8cd7d8791dd1e65937418
joseheco <jferhc@gmail.com>
2021-05-18 23:38:51 -0500

    class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Define class'''
+
+
+class Square:
+    '''Class
+    Attributes ___init___'''
+    def ___init___(self, size):
+        '''Initializes square size'''
+        self.__size = size
