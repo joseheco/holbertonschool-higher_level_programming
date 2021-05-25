eca182f40de700af0f4b0779ce2a51c4e28616e7
joseheco <jferhc@gmail.com>
2021-05-25 00:12:18 -0500

    rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
         """Private Attributes"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >=0")
         self.__width = value
 
@@ -35,6 +35,6 @@ class Rectangle:
         """Private Attributes"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >=0")
         self.__height = value
