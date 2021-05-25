f529aaeaa035ef11629cbbd3f9439cafd0f5fdb1
joseheco <jferhc@gmail.com>
2021-05-25 13:35:49 -0500

    rectangle

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -68,4 +68,4 @@ class Rectangle:
     
     def __repr__(self):
         """Return a string"""
-        return f"Rectangle({self.__width}, {self.__height})"
+        return "Rectangle({}, {})".format(self.__width, self.__height)
