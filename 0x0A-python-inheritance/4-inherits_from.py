307293904ca414ceb8134337852366df7d3d55a8
joseheco <jferhc@gmail.com>
2021-06-01 18:49:20 -0500

    fixchecker

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,4 +5,4 @@
 def inherits_from(obj, a_class):
     '''Function return True if its inherit_from
     , otherwise False'''
-    return issubclass(int, object) and (type(obj) != a_class)
+    return issubclass(type(obj), a_class) and (type(obj) != a_class)
