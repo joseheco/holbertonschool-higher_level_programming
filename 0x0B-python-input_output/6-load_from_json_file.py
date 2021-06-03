12c9cdf581e99e3d0931c2c6fccd5e2f194ab7b2
joseheco <jferhc@gmail.com>
2021-06-02 21:56:57 -0500

    Load_json

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ import json
 def load_from_json_file(filename):
     '''JSON'''
     with open(filename, 'r', encoding='utf-8') as f:
-        return json.load(f.read())
+        return json.loads(f.read())
