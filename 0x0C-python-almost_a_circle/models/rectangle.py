57a0fad1914542b682acdee956c1d60d93a39754
joseheco <jferhc@gmail.com>
2021-06-08 21:41:46 -0500

    fixing

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,9 +7,10 @@ from models.base import Base
 
 class Rectangle(Base):
     '''Rectangle class'''
-    def __init__(self, width, height, x=0, y=0, id=None)
-    suoer().__init__(id)
-    self.width = width
-    self.height = height
-    self.x = x
-    self.y = y
+    def __init__(self, width, height, x=0, y=0, id=None):
+        '''Rectangle'''
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
