bd5b4c1281e5167395403413e1f8b9f80d9637bf
joseheco <jferhc@gmail.com>
2021-06-08 21:33:01 -0500

    Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''First Rectangle'''
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    '''Rectangle class'''
+    def __init__(self, width, height, x=0, y=0, id=None)
+    suoer().__init__(id)
+    self.width = width
+    self.height = height
+    self.x = x
+    self.y = y
