38c902fe4a6b41d4c1d1c5eed0b5cdf80526eb18
joseheco <jferhc@gmail.com>
2021-06-08 23:39:29 -0500

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-'''Square'''
+'''Square class'''
 
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    '''Square'''
+    '''Square class'''
     def __init__(self, size, x=0, y=0, id=None):
-        '''Square'''
+        '''Square classt'''
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         '''string of square'''
-        return "[Square] ({}) {}/{} - {}".format(self.id,
+        return("[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x,
                                                  self.y,
-                                                 self.width)
+                                                 self.width))
