c5f85f1560a69eaf9528608bce1b3a994ed8b668
joseheco <jferhc@gmail.com>
2021-06-08 22:47:49 -0500

    str_pep8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,5 +77,8 @@ class Rectangle(Base):
 
     def __str__(self):
         '''str_self'''
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
-
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.__x,
+                                                       self.__y,
+                                                       self.__width,
+                                                       self.__height)
