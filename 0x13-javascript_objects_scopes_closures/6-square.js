803252e946ef1d1962776c0d6b240053076a9a5d
joseheco <jferhc@gmail.com>
2021-08-03 19:36:55 -0500

    JS

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const S = require('./5-square');
+class Square extends S {
+  charPrint (c) {
+    if (typeof (c) === 'undefined') {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
