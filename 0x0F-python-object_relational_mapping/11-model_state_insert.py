d48d89c7dd5ddc0d3753e49f3b77bbdbe35185ab
joseheco <jferhc@gmail.com>
2021-08-08 20:43:37 -0500

    Py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """script prints the State object with the name
 passed as argument from the database hbtn_0e_6_usa"""
-from model_state import Base, State
+from model_state  import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import sys
@@ -12,9 +12,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state_id = session.query(State.id).filter(
-        State.name == sys.argv[4]).first()
-    if state_id is None:
-        print("Not found")
-    else:
-        print("{:d}".format(state_id.id))
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
