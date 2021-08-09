8cba016a6e80ee64ed73c6c85b85e76a099ec9cb
joseheco <jferhc@gmail.com>
2021-08-08 20:23:25 -0500

    Py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""a python file that contains the class definition of a
-State"""
+""" python file that contains the class definition of a
+State and ain instance Base = declarative_base() """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
