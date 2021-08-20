f3979870fa4adaa7f5ee7601d3ac58a1e470cd73
joseheco <jferhc@gmail.com>
2021-08-19 22:30:31 -0500

    Sh

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Find a peak in a list"""
+
+
+def find_peak(list_of_integers):
+    """Find a peak in a list"""
+
+    if list_of_integers is None or list_of_integers == []:
+        return None
