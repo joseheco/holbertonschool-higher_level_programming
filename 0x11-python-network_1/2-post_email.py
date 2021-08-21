58586fd0579bffdfa59fd012e7880439db3881f5
joseheco <jferhc@gmail.com>
2021-08-20 19:36:13 -0500

    Python_net

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL and an email, sends a POST request to UR"""
+"""Python script that takes in a URL and email, sends a POST request to UR"""
 
 if __name__ == "__main__":
     from urllib import request, parse
