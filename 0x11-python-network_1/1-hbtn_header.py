30de40182bae2229e9df1c8c628edd00eed30a7d
joseheco <jferhc@gmail.com>
2021-08-20 19:29:26 -0500

    PN

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL, sends a request to the URL and displays"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        content = response.headers.get('X-Request-Id')
+        print(content)
