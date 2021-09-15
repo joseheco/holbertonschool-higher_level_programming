9e6250355e2bb1a0c17b1574437017c996ea2ed1
joseheco <jferhc@gmail.com>
2021-09-14 20:15:14 -0500

    JS

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request('http://swapi.co/api/films/' + process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(error);
+  }
+  console.log(JSON.parse(body).title);
+});
