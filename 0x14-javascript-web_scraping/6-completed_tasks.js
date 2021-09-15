0d13ae25e7fd4b00e87c63b14e221a4c2a10adac
joseheco <jferhc@gmail.com>
2021-09-14 21:30:11 -0500

    completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let completed = {};
+    for (let todo of body) {
+      if (todo.completed === true) {
+        if (completed[todo.userId] === undefined) {
+          completed[todo.userId] = 0;
+        }
+        completed[todo.userId]++;
+      }
+    }
+    console.log(completed);
+  }
+});
