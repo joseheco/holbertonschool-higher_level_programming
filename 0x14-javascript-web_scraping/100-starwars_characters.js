bcc490d61af732c4fce0c40ba819f7e306cd347d
joseheco <jferhc@gmail.com>
2021-09-15 19:31:12 -0500

    adv

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,13 +3,13 @@
 const request = require('request');
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (error, response, body) => {
-    if (error) {
-        console.error(error);
-    }
-    JSON.parse(body).characters.map((char) => {
-        request(char, (error, response, body) => {
-            if (error) console.log(error);
-            console.log(JSON.parse(body).name);
-        });
+  if (error) {
+    console.error(error);
+  }
+  JSON.parse(body).characters.map((char) => {
+    request(char, (error, response, body) => {
+      if (error) console.log(error);
+      console.log(JSON.parse(body).name);
     });
+  });
 });
