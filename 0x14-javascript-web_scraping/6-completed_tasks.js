4f36d174cdca16d0bf9879dea3e0e26c9c103861
joseheco <jferhc@gmail.com>
2021-09-15 19:14:25 -0500

    completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-request(process.argv[2], { json: true }, function (error, response, body) {
+const url = process.argv[2];
+request(url, { json: true }, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
